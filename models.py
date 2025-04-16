80da4bbfcdfdb6ea8a01669575b59b81958ec654
PauMartiNebot <58464638+Pauwn00@users.noreply.github.com>
2025-04-16 11:48:03 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-
+from sqlalchemy.ext.mutable import MutableList
 from sqlalchemy import Column, String
 from sqlalchemy.types import JSON
 from database import Base
@@ -8,6 +8,6 @@ class ParejaORM(Base):
 
     email = Column(String, primary_key=True, index=True)
     password = Column(String, nullable=False)
-    names = Column(JSON, default=[])
-    interests = Column(JSON, default=[])
-    likes = Column(JSON, default=[])
+    names = Column(MutableList.as_mutable(JSON), default=[])       
+    interests = Column(MutableList.as_mutable(JSON), default=[])   
+    likes = Column(MutableList.as_mutable(JSON), default=[])     
